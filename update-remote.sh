74d8ec8432b8c53897c73131c06eb0e9a4d1d6c4
nikita-bondarenko <your-email@example.com>
2025-07-20 12:10:43 +0300

    fix admin connection error

--- update-remote.sh
+++ update-remote.sh
@@ -1,3 +1,3 @@
 sh push.sh "$1"
 
-ssh root@81.177.165.71 "cd /home/rgp-frint && git pull origin main && docker-compose up -d --build --no-cache && docker-system prune -a -f"
+ssh root@81.177.165.71 "cd /home/rgp-frint && git pull origin main && docker-compose up -d --build  && docker-system prune -a -f"
